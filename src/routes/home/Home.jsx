90a6cb25f531246d47e0c65cdbdf047fcfa4e0c3
umarxon <anasxonovumarxon2000@mail.ru>
2023-09-23 15:30:54 +0500

    add cards

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from '../../components/header/Header'
+import CardItem1 from '../../components/cards/card_item1/CardItem1'
 
 function Home() {
     return (
         <div className='home'>
             <Header />
+            <CardItem1 />
+        
         </div>
     )
 }
