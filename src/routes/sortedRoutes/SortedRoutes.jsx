8e76cab0644b91d50cd41082225e8927a0dc1574
Muhammadsoleh <msoleh07@gmail.com>
2023-09-25 01:03:26 +0500

    soted page media add

--- src/routes/sortedRoutes/SortedRoutes.jsx
+++ src/routes/sortedRoutes/SortedRoutes.jsx
@@ -6,9 +6,9 @@ import Footer from '../../components/footer/Footer'
 function SortedRoutes() {
   return (
     <div className='sorted_routes'>
-        <Header/>
+        {/* <Header/> */}
         <Sorted/>
-        <Footer/>
+        {/* <Footer/> */}
     </div>
   )
 }
