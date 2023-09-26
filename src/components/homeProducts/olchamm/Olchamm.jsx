46df5822f4cc175a5c5f9a174b62cf754c3a2139
abdulloh <adxamovabdulloh06@gmail.com>
2023-09-26 15:08:49 +0500

    asdfg

---
+++ src/components/homeProducts/olchamm/Olchamm.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Olchamm.css'
+
+function Olchamm() {
+  return (
+    <div className='olchamm'>
+        <p>S - <span>44</span></p>
+        <p>M - <span>46</span></p>
+        <p>L - <span>48</span></p>
+        <p>XL - <span>50</span></p>
+        <p>XXL - <span>52</span></p>
+
+    </div>
+  )
+}
+
+export default Olchamm
---
+++ src/components/homeProducts/tarkib/Tarkib.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Tarkib.css'
+
+function Tarkib() {
+  return (
+    <div className='tarkibb'>
+        <p>Paxta <span>80%</span></p>
+        <p>Poliester <span>20%</span></p>
+    </div>
+  )
+}
+
+export default Tarkib
