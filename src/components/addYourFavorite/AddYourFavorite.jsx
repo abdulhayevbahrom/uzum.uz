ce79513e7d3230f826522d2e66ade4151a4938bd
Muhammadsoleh <msoleh07@gmail.com>
2023-09-26 09:49:22 +0500

    addat motion animated page

--- src/components/addYourFavorite/AddYourFavorite.jsx
+++ src/components/addYourFavorite/AddYourFavorite.jsx
@@ -1,40 +1,62 @@
-import React, {  memo, useState } from 'react'
+import { motion } from 'framer-motion'
+
+import React, { memo, useState , forwardRef} from 'react'
 import './AddYourFavorite.css'
 import { FiX } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
-import 'aos/dist/aos.css'
+
+
+const textAnimation = {
+  hidden: {
+    x: -100,
+    opacity: 0,
+  },
+  visible: custom => ({
+    x: 0,
+    opacity: 1,
+    transition: {delay: custom * .2}
+  })
+}
+
+
+
+
 
 function AddYourFavorite() {
   document.title = "Uzum - mahsulotlari kunning ertasiga yetkazib beriladigan ilk Oʻzbekiston savdo maydoni"
-
   const [addYourFovarite, setAddYourFovarite] = useState(false)
-  
 
   return (
-    <div className='add_your_favorite'>
-      <div className="img_border">
+    <motion.div
+      initial="hidden" 
+      whileInView="visible"
+      className='add_your_favorite'
+    >
+      <motion.div custom={1} variants={textAnimation} className="img_border">
         <img src="https://uzum.uz/static/img/hearts.cf414be.png" alt="" />
-      </div>
-      <h2>Sizga yoqqanini qoʻshing</h2>
-      <span>Mahsulotdagi ♡ belgisini bosing. Akkauntga kiring va barcha saralanganlar saqlanib qoladi</span>
-      <button onClick={() => setAddYourFovarite(!addYourFovarite , document.body.style.overflow = "hidden")}>
+      </motion.div>
+      <motion.h2 custom={2} variants={textAnimation}>Sizga yoqqanini qoʻshing</motion.h2>
+      <motion.span custom={3} variants={textAnimation}>Mahsulotdagi ♡ belgisini bosing. Akkauntga kiring va barcha saralanganlar saqlanib qoladi</motion.span>
+      <motion.button custom={4} variants={textAnimation} onClick={() => setAddYourFovarite(!addYourFovarite, document.body.style.overflow = "hidden")}>
         Akkauntga kirish
-      </button>
-      {addYourFovarite && (  
-        <div className="add_acount">
+      </motion.button>
+     {addYourFovarite &&(
+        <div 
+          className="add_acount"
+        >
           <div className="acoun_cart" >
-            <button onClick={() => setAddYourFovarite(!addYourFovarite , document.body.style.overflowY = "auto")} className='add_acount_none'>
+            <button onClick={() => setAddYourFovarite(!addYourFovarite, document.body.style.overflowY = "auto")} className='add_acount_none'>
               <FiX className='add_acount_none_x' />
             </button>
             <h2>Telefon raqamini kiriting</h2>
             <p>Tasdiqlash kodini SMS orqali yuboramiz</p>
             <input type="text" placeholder='+998 00 000 00 00' />
-            <input type="button" value="Kodni olish" className='btn'/>
+            <input type="button" value="Kodni olish" className='btn' />
             <span>Avtotizatsiyadan o'tish orqali siz <Link target='blank' to={'/privacy-policy-uz.html'}>shaxsiy ma'lumotlarni qayta ishlash siyosatiga rozilik bildirasiz</Link></span>
           </div>
         </div>
       )}
-    </div>
+    </motion.div>
   )
 }
 
