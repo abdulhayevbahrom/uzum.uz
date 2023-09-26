db9112d163a579a24f05cb0394a076bf3dbea190
Flowernoza <cadillac2111@Salmoorbeks-MacBook-Pro.local>
2023-09-26 12:34:14 +0500

    sidebar

--- src/components/header/Sidebar/Sidebar.jsx
+++ src/components/header/Sidebar/Sidebar.jsx
@@ -1,20 +1,68 @@
 import "./Sidebar.css";
-import { AiOutlineClose } from "react-icons/ai";
+import {
+  AiOutlineClose,
+  AiOutlineHeart,
+  AiOutlineQuestionCircle,
+} from "react-icons/ai";
 import React from "react";
 import { Link } from "react-router-dom";
 import BasicExample from "./HeaderAccordion";
+import { BsBagCheck } from "react-icons/bs";
+import { BsGeoAlt, BsEnvelope } from "react-icons/bs";
+import flag from "../../../assets/flag.png";
+import { AccordionSidebarLinks } from "./HeaderAccordion";
+import { TbBookDownload, TbMap } from "react-icons/tb";
 
-function Sidebar() {
+function Sidebar({ setOpenSidebar }) {
   return (
     <div className="Sidebar">
       <div className="sidebar-top">
-        <AiOutlineClose />
+        <AiOutlineClose onClick={() => setOpenSidebar(false)} />
         <div className="sidebar-top-register">
-          <Link>Kirish</Link>/ <Link>Ro'yxatdan o'tish</Link>
+          <Link>Kirish</Link> / <Link>Ro'yxatdan o'tish</Link>
         </div>
-        <div className="sidebar-katalog">
-          <BasicExample />
-        </div>
+      </div>
+      <div className="sidebar-katalog">
+        <BasicExample />
+      </div>
+      <div className="sidebar-links">
+        <Link to={"/"}>
+          <BsBagCheck />
+          Buyurtmalarim
+        </Link>
+        <Link to={"/heart"}>
+          <AiOutlineHeart />
+          Saralanganlar
+        </Link>
+        <button>
+          <BsGeoAlt />
+          Shahar: Toshkent
+        </button>
+        <Link to={"/delivery"} >
+          <TbMap />
+          Topshirish punkti
+        </Link>
+        <hr />
+        <Link to={"/"}>
+          <AiOutlineQuestionCircle />
+          Savol-javoblar
+        </Link>
+        <Link to={"/"}>
+          <BsEnvelope />
+          Biz bilan bog'laning
+        </Link>
+        <Link to={"/"}>
+          <TbBookDownload />
+          Uzum ilovasi
+        </Link>
+        <button className="sidebar-language">
+          <img className="flag" src={flag} alt="flag" />
+          Sayt tili: O'zbekcha
+        </button>
+        <hr />
+      </div>
+      <div className="AccordionSidebarLinks">
+        <AccordionSidebarLinks />
       </div>
     </div>
   );
