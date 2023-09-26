dbd8a1b32b5cb2394dfbbd765c8def9cdba4768e
Muhammadsoleh <msoleh07@gmail.com>
2023-09-26 12:07:47 +0500

    addat text animated

--- src/components/addYourFavorite/AddYourFavorite.jsx
+++ src/components/addYourFavorite/AddYourFavorite.jsx
@@ -1,7 +1,20 @@
 import React, { memo, useState } from 'react'
+import { motion } from 'framer-motion'
 import './AddYourFavorite.css'
 import { FiX } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
+
+const textAnimate = {
+  hidden: {
+    x: -100,
+    opacity: 0,
+  },
+  visible: custom => ({
+    x: 0,
+    opacity: 1,
+    transition: {delay: custom * .2}
+  })
+}
 
 
 function AddYourFavorite() {
@@ -9,15 +22,15 @@ function AddYourFavorite() {
   const [addYourFovarite, setAddYourFovarite] = useState(false)
 
   return (
-    <div className='add_your_favorite'>
-      <div className="img_border">
+    <motion.div initial='hidden' whileInView='visible' className='add_your_favorite'>
+      <motion.div custom={1} variants={textAnimate} className="img_border">
         <img src="https://uzum.uz/static/img/hearts.cf414be.png" alt="" />
-      </div>
-      <h2 >Sizga yoqqanini qoʻshing</h2>
-      <span>Mahsulotdagi ♡ belgisini bosing. Akkauntga kiring va barcha saralanganlar saqlanib qoladi</span>
-      <button onClick={() => setAddYourFovarite(!addYourFovarite, document.body.style.overflow = "hidden")}>
+      </motion.div>
+      <motion.h2 custom={2} variants={textAnimate} >Sizga yoqqanini qoʻshing</motion.h2>
+      <motion.span custom={3} variants={textAnimate}>Mahsulotdagi ♡ belgisini bosing. Akkauntga kiring va barcha saralanganlar saqlanib qoladi</motion.span>
+      <motion.button custom={4} variants={textAnimate} onClick={() => setAddYourFovarite(!addYourFovarite, document.body.style.overflow = "hidden")}>
         Akkauntga kirish
-      </button>
+      </motion.button>
       {addYourFovarite && (
         <div className="add_acount">
           <div className="acoun_cart" >
@@ -32,7 +45,7 @@ function AddYourFavorite() {
           </div>
         </div>
       )}
-    </div>
+    </motion.div>
   )
 }
 
