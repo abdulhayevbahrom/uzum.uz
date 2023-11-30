828a47f8479f5b7798a279d5f7aba49a11cb900e
Flowernoza <cadillac2111@Salmoorbeks-MacBook-Pro.local>
2023-11-30 15:29:54 +0500

    first commit

--- src/routes/cart/Cart.jsx
+++ src/routes/cart/Cart.jsx
@@ -1,31 +1,25 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import Header from '../../components/header/Header';
-import PopularProducts from '../../components/popularProducts/PopularProducts';
-import Footer from '../../components/footer/Footer';
-import AddProducts from '../../components/addProducts/AddProducts';
-import CartProducts from '../../components/cartProducts/CartProducts';
+import React from "react";
+import { useSelector } from "react-redux";
+import Header from "../../components/header/Header";
+import PopularProducts from "../../components/popularProducts/PopularProducts";
+import Footer from "../../components/footer/Footer";
+import AddProducts from "../../components/addProducts/AddProducts";
+import CartProducts from "../../components/cartProducts/CartProducts";
 
 function Cart() {
-    let cartData = useSelector((s) => s.addToCart)
+  let cartData = useSelector((s) => s.addToCart);
 
-
-    document.title = "Savat - Uzum"
-    return (
-        <div className='cart'>
-            <div>
-                <Header />
-                {
-                    cartData.length ?
-                        <CartProducts data={cartData} />
-                        :
-                        <AddProducts />
-                }
-                <PopularProducts />
-                <Footer />
-            </div>
-        </div>
-    )
+  document.title = "Savat - Uzum";
+  return (
+    <div className="cart">
+      <div>
+        <Header />
+        {cartData.length ? <CartProducts data={cartData} /> : <AddProducts />}
+        <PopularProducts />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
